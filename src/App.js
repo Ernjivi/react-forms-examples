bb77be967b6aa5a77ca44b5c2916c92a2cd0be8b
Ernesto Jimenez V <ernesto@bedu.org>
2019-10-16 12:39:58 -0500

    Added example using component components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    firstName: ''
+  }
+
+  onChange = (evt) => {
+
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+
+  } 
+  
+  onSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input type="text" name="firstName" onChange={this.onChange} value={this.state.firstName} />
+        <input type="submit" value="enviar" />
+      </form>
+    );
+  }
 }
 
 export default App;
